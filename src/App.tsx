1911715dd2e4f0f04ff66c196254bae85ca1a55c
AITBENADDI Youssef <youssefaitbenaddi8@gmail.com>
2025-08-30 11:02:18 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -611,7 +611,7 @@ function App() {
                             })}
                           </td>
                           <td className="px-6 py-4 text-lg font-bold text-red-600">
-                            MAD{expense.amount.toLocaleString()}
+                            mad{expense.amount.toLocaleString()}
                           </td>
                           <td className={`px-6 py-4 text-sm ${isDarkMode ? 'text-slate-300' : 'text-slate-700'} leading-relaxed`}>
                             {expense.description}
